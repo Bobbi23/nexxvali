661d46856cb350970bfe3b8e40723b805185d3fd
kamy <imamkami6@gmail.com>
2023-11-20 21:17:27 -0500

    first commit

---
+++ src/test/java/nexxvali/JenkinProjectdemo/Jenkinsdemo.java
@@ -0,0 +1,12 @@
+package nexxvali.JenkinProjectdemo;
+
+import org.testng.annotations.Test;
+
+public class Jenkinsdemo {
+	@Test
+	public void jenkins () {
+		System.out.println("jenkins learning is fun");
+	}
+	
+
+}
